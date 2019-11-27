1242ab7b1b3a46774ef35945b2df6465f1d540c2
wcwright124 <wcw124@psu.edu>
2019-11-27 16:22:10 -0500

    Added script to validate symbol table after insertion

---
+++ test_verify_symbol_table.py
@@ -0,0 +1,27 @@
+import MySQLdb as mdb
+
+key_file = open('sec_user_pass.txt', 'r')
+DB_PASSWORD = key_file.readline().rstrip()
+key_file.close()
+
+def check_securities_master():
+    db_host = 'localhost'
+    db_user = 'sec_user'
+    db_pass = DB_PASSWORD
+    db_name = 'securities_master'
+    con = mdb.connect(
+        host = db_host,
+        user = db_user,
+        passwd = db_pass,
+        db = db_name
+    )
+    query = "SELECT * FROM symbol"
+    cur = con.cursor()
+    cur.execute(query)
+    res = cur.fetchall()
+
+    for r in res:
+        print(r)
+
+if __name__ == '__main__':
+    check_securities_master()
